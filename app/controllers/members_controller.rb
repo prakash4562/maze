6c621bc98f4b17784f7fd9bd613ef93c27df3f47
chitranshoo <chitranshoo10081998@gmail.com>
2022-05-17 11:18:04 +0530

    committed

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,36 +1,56 @@
 class MembersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-
-    @users = User.order(created_at: :desc)
-
+    if current_user.roles.first.name == "admin"
+      @users = User.order(created_at: :desc)
+    else
+      redirect_to posts_path
+    end
   end
 
   def edit
-    @user = User.find(params[:id])
+    if current_user.roles.first.name == "admin"
+      @user = User.find(params[:id])
+    else
+      redirect_to posts_path
+    end
   end
 
   def new
-    @user = User.new
+    if current_user.roles.first.name == "admin"
+      @user = User.new
+    else
+      redirect_to posts_path
+    end
   end
 
   def create
-    @user = User.new(user_params)
-    @user.add_role params[:roles]
-    if @user.save!
-      redirect_to posts_path
+    if current_user.roles.first.name == "admin"
+      @user = User.new(user_params)
+      @user.add_role params[:roles]
+      if @user.save!
+        redirect_to posts_path
+      else
+        redirect_to new
+      end
     else
-      redirect_to new
+      redirect_to posts_path
     end
   end
 
   def ban
-    @user = User.find(params[:id])
-    if @user.access_locked?
-      @user.unlock_access!
+    if current_user.roles.first.name == "admin"
+      @user = User.find(params[:id])
+      if @user.access_locked?
+        @user.unlock_access!
+      else
+        @user.lock_access!
+      end
+      redirect_to members_path
     else
-      @user.lock_access!
+      redirect_to posts_path
     end
-    redirect_to members_path
   end
 
   private
