dfaaae7f12f390b9e9a18a1fa4d3f7f273997bb1
chitranshoo <chitranshoo10081998@gmail.com>
2022-05-18 12:14:26 +0530

    like polymorphic commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,5 @@ class Post < ApplicationRecord
   validates_presence_of :content
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  has_many :likes, as: :likeable, dependent: :destroy
 end
