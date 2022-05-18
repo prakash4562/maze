4b6cc11e93ffcd80b4e9b60014c7cdd788ca00ee
chitranshoo <chitranshoo10081998@gmail.com>
2022-05-18 16:49:12 +0530

    admin delete commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,12 +14,18 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  VALID_NUMBER_REGEX=/\A^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[789]\d{9}$\z/i
+  validates :number, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 10 ,maximum: 10 },
+            format: { with: VALID_NUMBER_REGEX }
+
 
 
   after_create :assign_default_role
 
   def assign_default_role
-    self.add_role(:user) if self.roles.blank?
+    self.add_role(:newuser) if self.roles.blank?
   end
 
 end
